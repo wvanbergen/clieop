1cfd86dacb21b84fd18ec2f680f725b958444756
Leon Berenschot <leonb@beriedata.nl>
2010-03-16 15:59:42 +0100

    ability to specify transaction_type on record

--- lib/clieop/batch.rb
+++ lib/clieop/batch.rb
@@ -53,7 +53,7 @@ module Clieop
         total_amount  += (tr[:amount].to_f * 100).truncate
 
         # prepare data for this transaction's records
-        transaction_type = @batch_info[:transaction_group] == 10 ? 1002 : 0
+        transaction_type = tr[:transaction_type] || (@batch_info[:transaction_group] == 10 ? 1002 : 0)
         to_account       = @batch_info[:transaction_group] == 10 ? @batch_info[:account_nr] : tr[:account_nr]
         from_account     = @batch_info[:transaction_group] == 10 ? tr[:account_nr] : @batch_info[:account_nr]
         amount_in_cents  = (tr[:amount].to_f * 100).truncate
