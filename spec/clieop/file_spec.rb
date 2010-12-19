b84faa277e7e210fc9bdd3e181a035892db7cfa5
Willem van Bergen <willem@vanbergen.org>
2010-12-19 16:51:43 -0500

    Updated the File spec to not rely on a specific date.

--- spec/clieop/file_spec.rb
+++ spec/clieop/file_spec.rb
@@ -7,10 +7,8 @@ describe Clieop::File do
     it "should create 'filename' and put the CLIEOP data in it" do
       file = mock('file')
       File.should_receive(:open).with("filename", "w").and_yield(file)
-      file.should_receive(:write).with("0001A091210CLIEOP03         1                     \r\n9999A                                             \r\n")
+      file.should_receive(:write).with("0001A#{Date.today.strftime('%d%m%y')}CLIEOP03         1                     \r\n9999A                                             \r\n")
       subject.save('filename')
     end
-
   end
-
 end
